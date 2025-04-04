144da589655cfb6bf71878da688451d61648df0f
Nicholas Pelliccia <87953745+ogpelliccia@users.noreply.github.com>
2025-04-04 13:29:11 -0400

    Update source code for v0.4.5

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 //Drop The Number
 				  //by Nicholas Pelliccia
 //Start Date:			    Feb. 16, 2025
-//Revision Date:		    Mar. 20, 2025
+//Revision Date:		    Apr. 04, 2025
+
+//Changes since last revision:
+//	complete overhaul of tile height checking system using placeTile
+//  removed gridHeight vector
+//  moved merging logic to new function checkColumn
+//	fixed left and right merging
+//	added logic for multiple merges, using recursion of checkColumn
+//  partially added logic for combo merges (left and bottom have same value as placed tile)
+//  TODO: implement game state structure
+//	TODO: implement simple "animation" for multiple merging
+//	TODO: fully implement combo merges
+//	TODO: implement scoring system
 
 #include <SFML/Graphics.hpp>  //The main SFML Graphics header
 #include <iostream>			  //This is used for debug outputs to the console
-#include <vector>			  //Vectors are used for the tile grid and the gridHeight tracker
+#include <vector>			  //Vectors are used for the tile grid
 #include <random>			  //This is to generate a random texture and value for a tile
 #include <string>			  //This is also used for console debugging purposes and the score text
 #include <cmath>			  //pow() is used to set the value of each tile
@@ -21,15 +33,123 @@ const int spriteTextures[18] = { 0, 101, 203, 305, 407, 509, 611, 713, 815, 917,
 struct Tile 
 {
 	Sprite sprite; //Texture of the tile
-	Tile* left;	   //Pointer to left tile
-	Tile* right;   //Pointer to right tile
-	Tile* bottom;  //Pointer to bottom tile
 	int value;	   //Value of the tile corresponding to its texture (this might be removed in the near future)
 
 	//The argument list for initializing a new tile
 	Tile(Sprite inputSprite, int val) 
-		: sprite(inputSprite), value(val), left(nullptr), right(nullptr), bottom(nullptr) {}
+		: sprite(inputSprite), value(val) {}
 };
+
+//Is used to calculate the y-position for the tile to drop
+//New in 0.5: this function replaces the gridHeight vector
+int countTiles(std::vector<std::vector<Tile>>& grid, int column)
+{
+	int count = 0;	//The number of non-zero blocks
+
+	for (int i = 0; i < grid[column].size(); i++)
+	{
+		if (grid[column][i].value != 0)
+		{
+			count++;
+		}
+	}
+
+	return count;
+}
+
+void checkColumn(std::vector<std::vector<Tile>>&grid, int column, int &scoreAdd)
+{
+	int top = 0;
+	int spriteTexture = 0;
+
+	for (int i = 5; i >= 0; i--)
+	{
+		if (top == 0 && grid[column][i].value != 0)
+		{
+			top = i;
+		}
+	}
+	//std::cout << top << std::endl;
+
+	//NOTE: The left and right merging is very buggy and needs improvement. For the sake of functionality, these features are disabled for now
+	//Once everything works, these features will be enabled
+	for (int i = 0; i < grid[column].size(); i++)
+	{
+		//Check for Left combo merge (left and bottom tiles are same)
+		std::cout << (i > 0 && grid[column][i].value != 0 && grid[column][i].value == grid[column][i - 1].value && grid[column][i].value == grid[column - 1][i].value) << std::endl;
+		if (i > 0 && grid[column][i].value != 0 && grid[column][i].value == grid[column][i - 1].value && grid[column][i].value == grid[column - 1][i].value)
+		{
+			spriteTexture = static_cast<int>(log2(grid[column][i - 1].value));
+			grid[column][i - 1].value *= 4;
+			grid[column][i - 1].sprite.setTextureRect(IntRect(spriteTextures[spriteTexture], 0, 102, 100));
+
+			grid[column][i].value = 0;
+			grid[column][i].sprite.setPosition(0.0f, 0.0f);
+
+			grid[column - 1][i].value = 0;
+			grid[column - 1][i].sprite.setPosition(0.0f, 0.0f);
+
+			scoreAdd += grid[column][i].value;
+			checkColumn(grid, column, scoreAdd);
+		}
+
+		//Check for bottom merge
+		else if (i < 5 && grid[column][i].value != 0 && grid[column][i].value == grid[column][i + 1].value)
+		{
+			spriteTexture = static_cast<int>(log2(grid[column][i].value));
+			grid[column][i].value *= 2;
+			grid[column][i].sprite.setTextureRect(IntRect(spriteTextures[spriteTexture], 0, 102, 100));
+
+			for (int j = i + 1; j < countTiles(grid, column); j++)
+			{
+				grid[column][j] = grid[column][j + 1];
+				grid[column][j].sprite.setPosition(grid[column][j].sprite.getPosition().x, grid[column][j].sprite.getPosition().y + 102);
+			}
+			scoreAdd += grid[column][i].value;
+			checkColumn(grid, column, scoreAdd);
+		}
+
+		//Check for tile sliding after left or right merging
+		else if (grid[column][i].value == 0 && i < top)
+		{
+			for (int j = i + 1; j < countTiles(grid, column) + 1; j++)
+			{
+				grid[column][j - 1] = grid[column][j];
+				grid[column][j - 1].sprite.setPosition(grid[column][j].sprite.getPosition().x, grid[column][j].sprite.getPosition().y + 102);
+			}
+		}
+
+		//Check for left merge
+		else if (column > 0 && grid[column][i].value != 0 && grid[column - 1][i].value == grid[column][i].value)
+		{
+			spriteTexture = static_cast<int>(log2(grid[column][i].value));
+			grid[column - 1][i].value = 0;
+			grid[column - 1][i].sprite.setPosition(0.0f, 0.0f);
+			checkColumn(grid, column - 1, scoreAdd);
+
+			grid[column][i].value *= 2;
+			grid[column][i].sprite.setTextureRect(IntRect(spriteTextures[spriteTexture], 0, 102, 100));
+			scoreAdd += grid[column][i].value;
+
+			checkColumn(grid, column, scoreAdd);
+		}
+
+		//Check for right merge
+		else if (column < 4 && grid[column][i].value != 0 && grid[column + 1][i].value == grid[column][i].value)
+		{
+			spriteTexture = static_cast<int>(log2(grid[column][i].value));
+			grid[column + 1][i].sprite.setPosition(0.0f, 0.0f);
+			grid[column + 1][i].value = 0;
+			checkColumn(grid, column + 1, scoreAdd);
+
+			grid[column][i].value *= 2;
+			grid[column][i].sprite.setTextureRect(IntRect(spriteTextures[spriteTexture], 0, 102, 100));
+			scoreAdd += grid[column][i].value;
+
+			checkColumn(grid, column, scoreAdd);
+		}
+	}
+}
 
 //Spawns a new block
 void resetSprite(Tile& currentTile, int& column, int& textureIndex)
@@ -44,175 +164,37 @@ void resetSprite(Tile& currentTile, int& column, int& textureIndex)
 }
 
 //Places the tile into a fixed spot on the grid and performs merging
-void placeTile(std::vector<std::vector<Tile>>& grid, Tile& currentTile, std::vector<float>& gridHeight, int& column, int& textureIndex, int& score)
+void placeTile(std::vector<std::vector<Tile>>& grid, Tile& currentTile, int& column, int& textureIndex, int& score)
 {
-	int row = (gridHeight[column] - 90) / 102;		//This is for easy navigation of the grid vector
+	int row = countTiles(grid, column);				//This is for easy navigation of the grid vector
 	int scoreAdd = 0;								//This is added to the game score at the end of the function
-	bool mergeLeft = false;							//A flag for whether a left merge needs to be executed
-	bool mergeRight = false;						//A flag for whether a right merge needs to be executed
-	bool mergeBottom = false;						//A flag for whether a bottom merge needs to be executed
-	bool merged = true;								//A boolean meant to continue checking for merging
 
 	//The current tile has its texture and position set
-	grid[column][row].sprite.setTextureRect(IntRect(spriteTextures[textureIndex], 0, 102, 100));
-	grid[column][row].sprite.setPosition(currentTile.sprite.getPosition().x, gridHeight[column]);
-
-	//Checking for bottom, right, and left tiles
-	if (row > 0)
-	{
-		grid[column][row].bottom = &grid[column][row - 1];
-	}
-	if (column < 4)
-	{
-		grid[column][row].right = &grid[column + 1][row];
-	}
-	if (column > 0)
-	{
-		grid[column][row].left = &grid[column - 1][row];
-	}
-
-	grid[column][row].value = currentTile.value;	//currentTile's value is moved to the grid vector
-	gridHeight[column] -= 102;						//Every time a tile is placed, the grid height is increased (which decreases its drop position)
+	grid[column][row].value = currentTile.value;		//currentTile's value is moved to the grid vector
+	grid[column][row].sprite.setTextureRect(IntRect(spriteTextures[static_cast<int>(log2((double)grid[column][row].value)) - 1], 0, 102, 100));
+	grid[column][row].sprite.setPosition(currentTile.sprite.getPosition().x, (600 - (countTiles(grid, column) * 102)) + 102);
 
 	//Console Debug Statements
 	std::cout << "Row Index: " << row << std::endl;
 	std::cout << "Column Index: " << column << std::endl;
 	std::cout << grid[column][row].value << ", " << grid[column][row + 1].value << std::endl;
 
-	//Checks for merging (has no purpose at the moment)
-	if (grid[column][row].left != nullptr && grid[column][row].left->value > 0 && grid[column][row].left->value == grid[column][row].value)
-	{
-		mergeLeft = true;
-	}
-
-	if (grid[column][row].right != nullptr && grid[column][row].right->value > 0 && grid[column][row].right->value == grid[column][row].value)
-	{
-		mergeRight = true;
-	}
-
-	if (grid[column][row + 1].value > 0 && grid[column][row + 1].value == grid[column][row].value)
-	{
-		mergeBottom = true;
-	}
-
-	//NOTE: The left and right merging is very buggy and needs improvement. For the sake of functionality, these features are disabled for now
-	//Once everything works, these features will be enabled
-
-	/*if (mergeLeft)
-	{
-		std::cout << std::to_string(grid[column][row].left->value) << std::endl << std::to_string(currentTile.value) << std::endl << std::endl << std::endl;
-		grid[column][row].left->sprite.setPosition(0.0f, 0.0f);
-		grid[column][row].left->value = 0;
-		gridHeight[column - 1] += 102;
-		textureIndex += 1;
-		grid[column][row].sprite.setTextureRect(IntRect(spriteTextures[textureIndex], 0, 102, 100));
-		grid[column][row].value *= 2;
-		scoreAdd += grid[column][row].value;
-
-		if (grid[column - 1][row - 1].value != 0)
-		{
-			while (grid[column - 1][row].value == 0)
-			{
-				for (int i = row; i > 0; i--)  // Start from row 1 (above the base row)
-				{
-					grid[column - 1][i] = grid[column - 1][i - 1];
-					grid[column - 1][i].sprite.setPosition(grid[column + 1][i].sprite.getPosition().x, grid[column + 1][i].sprite.getPosition().y + 102);
-
-					//if(gridHeight[column] < 600) { gridHeight[column] += 102; }
-
-					grid[column - 1][0].value = 0;  // Clear the top-most space
-				}
-			}
-		}
-		merged = true;
-	}
-
-	if (mergeRight)
-	{
-		std::cout << std::to_string(grid[column][row].right->value) << std::endl << std::to_string(currentTile.value) << std::endl << std::endl << std::endl;
-		grid[column][row].right->sprite.setPosition(0.0f, 0.0f);
-		grid[column][row].right->value = 0;
-		gridHeight[column + 1] += 102;
-		textureIndex += 1;
-		grid[column][row].sprite.setTextureRect(IntRect(spriteTextures[textureIndex], 0, 102, 100));
-		grid[column][row].value *= 2;
-		scoreAdd += grid[column][row].value;
-
-		while (grid[column + 1][row].value == 0)
-		{
-			for (int i = row; i > 0; i--)  // Start from row 1 (above the base row)
-			{
-				grid[column + 1][i] = grid[column + 1][i - 1];
-				grid[column + 1][i].sprite.setPosition(grid[column + 1][i].sprite.getPosition().x, grid[column + 1][i].sprite.getPosition().y + 102);
-
-				//if(gridHeight[column] < 600) { gridHeight[column] += 102; }
-					
-				grid[column + 1][0].value = 0;  // Clear the top-most space
-			}
-		}
-		merged = true;
-	}*/
-
-	//Bottom merging works perfectly and is thus enabled
-	if (mergeBottom)
-	{
-		//Debug for the values of the current tile and its bottom tile
-		std::cout << std::to_string(grid[column][row + 1].value) << std::endl << std::to_string(grid[column][row].value) << std::endl << std::endl << std::endl;
-
-		//Check for same values and out-of-bounds, which is repeated in the case of a stack with increasingly doubled values (Ex (from top to bottom): 128,256,512)
-		while (row < 5 && grid[column][row + 1].value == grid[column][row].value)
-		{
-			//The position and value are set
-			grid[column][row].sprite.setPosition(0.0f, 0.0f);
-			grid[column][row].value = 0;
-
-			gridHeight[column] += 102;	//Since the stack height decreases, the y-position increases
-			
-			//The new merged tile's value is doubled
-			textureIndex += 1;
-			grid[column][row + 1].sprite.setTextureRect(IntRect(spriteTextures[textureIndex], 0, 102, 100));
-			grid[column][row + 1].value *= 2;
-
-			scoreAdd += grid[column][row + 1].value;	//The score increases due to the merge
-			row += 1;									//The row increases in the case of a repeat
-
-			merged = true;								//This boolean will be useful with repeated merges
-		}
-	}
-
-	//Booleans are set based on merging conditions (this is not really useful right now)
-	if (grid[column][row].left && grid[column][row].left->value != grid[column][row].value)
-	{
-		mergeLeft = false;
-	}
-	if (grid[column][row].right && grid[column][row].right->value != grid[column][row].value)
-	{
-		mergeRight = false;
-	}
-	if (grid[column][row + 1].value != grid[column][row].value)
-	{
-		mergeBottom = false;
-	}
+	//Checks for merging
+	checkColumn(grid, column, scoreAdd);
 
 	score += scoreAdd;										//The score is increased if merging occurs
 	resetSprite(currentTile, column, textureIndex);			//After the current tile is dropped, a new one is spawned
 }
 
 //The game area is reset for a new game
-void initializeGame(std::vector<std::vector<Tile>>& grid, std::vector<float>& gridHeight, Tile& currentTile, int& column, bool& start, Texture& texture, int& textureIndex, int& score)
+void initializeGame(std::vector<std::vector<Tile>>& grid, Tile& currentTile, int& column, bool& start, Texture& texture, int& textureIndex, int& score)
 {
-	//The gridHeight values are reset to their initial values
-	for (int i = 0; i < gridHeight.size(); i++)
-	{
-		gridHeight[i] = 600.0f;
-	}
-
 	//The grid is populated with empty tiles
 	for (int i = 0; i < 5; i++)
 	{
 		for (int j = 0; j < 6; j++)
 		{
-			grid[i].push_back(Tile(Sprite(texture), 0));
+			grid[i][j].value = 0;
 			grid[i][j].sprite.setPosition(0.0f, 0.0f);
 		}
 	}
@@ -233,9 +215,23 @@ void initializeGame(std::vector<std::vector<Tile>>& grid, std::vector<float>& gr
 
 int main()
 {
-	//const int horizontalPos[5] = { 45, 147, 249, 351, 453 };						//These are the x-coordinates for each column
-	std::vector<float> gridHeight = { 600.0f, 600.0f, 600.0f, 600.0f, 600.0f };		//These are y-coordinates to place the tile based on the stack height
-	std::vector<std::vector<Tile>> grid = { {}, {}, {}, {}, {} };					//The grid keeps track of tiles and their values and positions
+	//The texture file for the tiles is loaded
+	Texture texture;
+	texture.loadFromFile("Texture-Map.png");
+
+	//The texture for the background is loaded
+	Texture bg;
+	bg.loadFromFile("Layout.png");
+
+	//const int horizontalPos[5] = { 45, 147, 249, 351, 453 };											//These are the x-coordinates for each column
+	std::vector<std::vector<Tile>> grid = { {}, {}, {}, {}, {} };										//The grid keeps track of tiles and their values and positions
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			grid[i].push_back(Tile(sf::Sprite(texture), 0));
+		}
+	}
 	int column = 0;				//Column counter
 	int score = 0;				//The score in the game
 	int textureIndex = 0;		//The index for the global spriteTextures array
@@ -248,14 +244,6 @@ int main()
 
 	//The main window the game is contained in
 	RenderWindow window(VideoMode(600, 800), "Drop The Number!");
-
-	//The texture file for the tiles is loaded
-	Texture texture;
-	texture.loadFromFile("Texture-Map.png");
-
-	//The texture for the background is loaded
-	Texture bg;
-	bg.loadFromFile("Layout.png");
 
 	//The current tile is initialized and spawned
 	Tile currentTile(Sprite(texture), 0);
@@ -300,7 +288,7 @@ int main()
 	window.setFramerateLimit(60);		//The game's framerate is capped at 60 FPS (a higher framerate may make the game more difficult)
 
 	//A new game is started
-	initializeGame(grid, gridHeight, currentTile, column, start, texture, textureIndex, score);
+	initializeGame(grid, currentTile, column, start, texture, textureIndex, score);
 
 	while (window.isOpen())
 	{
@@ -317,7 +305,7 @@ int main()
 			if (e.type == Event::KeyPressed)
 			{
 				//When the Right Arrow or D keys are pressed, the current tile moves 1 column to the right
-				if (currentTile.sprite.getPosition().x < 453 && currentTile.sprite.getPosition().y < gridHeight[column] && !paused &&
+				if (currentTile.sprite.getPosition().x < 453 && currentTile.sprite.getPosition().y < 600 - (countTiles(grid, column) * 102) && !paused &&
 					(e.key.code == sf::Keyboard::Right || e.key.code == sf::Keyboard::D))
 				{
 					currentTile.sprite.setPosition(currentTile.sprite.getPosition().x + 102, currentTile.sprite.getPosition().y);
@@ -325,7 +313,7 @@ int main()
 				}
 
 				//When the Left Arrow or A keys are pressed, the current tile moves 1 column to the left
-				if (currentTile.sprite.getPosition().x > 45 && currentTile.sprite.getPosition().y < gridHeight[column] && !paused &&
+				if (currentTile.sprite.getPosition().x > 45 && currentTile.sprite.getPosition().y < 600 - (countTiles(grid, column) * 102) && !paused &&
 					(e.key.code == sf::Keyboard::Left || e.key.code == sf::Keyboard::A))
 				{
 					currentTile.sprite.setPosition(currentTile.sprite.getPosition().x - 102, currentTile.sprite.getPosition().y);
@@ -333,15 +321,15 @@ int main()
 				}
 
 				//When the Down Arrow or S keys are pressed, the current tile is dropped
-				if (currentTile.sprite.getPosition().y <= gridHeight[column] && !paused && (e.key.code == sf::Keyboard::Down || e.key.code == sf::Keyboard::S))
+				if (currentTile.sprite.getPosition().y <= 600 - (countTiles(grid, column) * 102) && !paused && (e.key.code == sf::Keyboard::Down || e.key.code == sf::Keyboard::S))
 				{
-					placeTile(grid, currentTile, gridHeight, column, textureIndex, score);
+					placeTile(grid, currentTile, column, textureIndex, score);
 				}
 				
 				//When the game over screen is displayed, pressing F2 starts a new game
 				if (game == false && e.key.code == sf::Keyboard::F2)
 				{
-					initializeGame(grid, gridHeight, currentTile, column, start, texture, textureIndex, score);
+					initializeGame(grid, currentTile, column, start, texture, textureIndex, score);
 					game = true;
 				}
 
@@ -361,14 +349,14 @@ int main()
 		}
 		if (!paused)
 		{
-			if (currentTile.sprite.getPosition().y < gridHeight[column])
+			if (currentTile.sprite.getPosition().y < 600 - (countTiles(grid, column) * 102))
 			{
 				currentTile.sprite.move(0.0f, 0.5f);	//The current tile moves downward at a certain speed
 			}
-			else if (currentTile.sprite.getPosition().y <= gridHeight[column])
+			else if (currentTile.sprite.getPosition().y <= 600 - (countTiles(grid, column) * 102))
 			{
 				//When the current tile lands on top of a stack, it's attributes are transferred to the grid vector
-				placeTile(grid, currentTile, gridHeight, column, textureIndex, score);
+				placeTile(grid, currentTile, column, textureIndex, score);
 			}
 			else if (game)
 			{
@@ -385,9 +373,10 @@ int main()
 			//std::cout << "Time Elapsed: " << clock.getElapsedTime().asSeconds() << std::endl;
 			//std::cout << "Game Status: " << game << std::endl;
 
-			//std::cout << "Column Index: " << column << std::endl;
-			std::cout << "Grid Height: " << gridHeight[column] << std::endl;
+			std::cout << "Column Index: " << column << std::endl;
+			std::cout << "Grid Height: " << 600 - (countTiles(grid, column) * 102) << std::endl;
 			//std::cout << "Tile Value: " << currentTile.value << std::endl;
+			//std::cout << std::to_string(static_cast<int>(log2(currentTile.value))) << std::endl;
 
 			std::cout << "Grid Array: " << std::endl;
 			for (int i = 0; i < 5; i++)
@@ -413,7 +402,7 @@ int main()
 			{
 				for (int j = 0; j < 6; j++)
 				{
-					if (grid[i][j].sprite.getPosition().x != 0)
+					if (grid[i][j].value != 0)
 					{
 						window.draw(grid[i][j].sprite);
 					}
